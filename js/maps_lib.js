2cbaf3452b867353092ebe46fc3e669352785c6d
kevinrak9 <rak.kevin@gmail.com>
2015-11-19 22:33:07 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -167,9 +167,18 @@
         var type_column = "'Mental Health Services - Population Served'";
         var tempWhereClause = [];
         if ( $("#cbType1").is(':checked')) tempWhereClause.push("Adults");
+        self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";   
+        
+        var type_column = "'Mental Health Services - Population Served'";
+        var tempWhereClause = [];
         if ( $("#cbType2").is(':checked')) tempWhereClause.push("Youth");
+        self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";   
+        
+        var type_column = "'Mental Health Services - Population Served'";
+        var tempWhereClause = [];
         if ( $("#cbType3").is(':checked')) tempWhereClause.push("Families");
-        self.whereClause += " OR " + type_column + " IN ('" + tempWhereClause.join("','") + "')";   
+        self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";   
+
         //-----end of custom filters-----
 
         self.getgeoCondition(address, function (geoCondition) {
